aba21ecfa65d471f025ffd2e05cbc1a5d510bc3c
Nathanvititoe <nathanvititoe@gmail.com>
2023-06-16 11:28:35 -0700

    test

--- public/app.js
+++ public/app.js
@@ -4,7 +4,7 @@ let user = 'nathanvititoe';
 
 async function JSONdata() {
   try {
-    const response = await fetch(`${url}/${route}/${user}`);
+    const response = await fetch(`${url}/cards/nathanvititoe`);
     const data = await response.json();
     console.log(data);
   } catch (err) {
