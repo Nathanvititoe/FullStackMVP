29c8cd3b4467f1e25c57ea396b537f8d2dd4d4bd
Nathanvititoe <nathanvititoe@gmail.com>
2023-06-16 11:24:24 -0700

    adjusted fetch

--- public/app.js
+++ public/app.js
@@ -1,8 +1,9 @@
-const url = 'https://mvpwebservice.onrender.com'
+const url = 'https://mvpwebservice.onrender.com';
+const route = 'nathanvititoe';
 
 async function JSONdata() {
   try {
-    const response = await fetch(`${url}`);
+    const response = await fetch(`${url}/${route}`);
     const data = await response.json();
     console.log(data);
   } catch (err) {
